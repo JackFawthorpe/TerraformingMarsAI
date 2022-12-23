9eb0cbf2ffab99584242003027d6bb7055be4912
Emma Fawthorpe <emmacansew@gmail.com>
2022-12-23 22:29:18 +1300

    Created Solar Power Card

--- src/main/Managers/CardManager.java
+++ src/main/Managers/CardManager.java
@@ -50,6 +50,7 @@ public class CardManager {
         if (base) {
             deck.add(new BreathingFiltersCard(manager));
             deck.add(new LunarBeamCard(manager));
+            deck.add(new SolarPowerCard(manager));
         }
 
         if (corporate) {
