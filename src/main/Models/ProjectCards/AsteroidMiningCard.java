558be75fa5115824de4dd42768bef135e4574767
Emma Fawthorpe <emmacansew@gmail.com>
2022-12-24 18:52:32 +1300

    Created Asteroid Ming Card

---
+++ src/main/Models/ProjectCards/AsteroidMiningCard.java
@@ -0,0 +1,35 @@
+package main.Models.ProjectCards;
+
+import main.Enums.DLC;
+import main.Enums.Resource;
+import main.Enums.Tag;
+import main.Exceptions.InvalidPlayerTransactionException;
+import main.Managers.CardManager;
+import main.Models.BaseCard;
+import main.Models.PlayerTransaction;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AsteroidMiningCard extends BaseCard {
+    public AsteroidMiningCard(CardManager cardManager){
+        this.cardManager = cardManager;
+        title = "Asteroid Mining";
+        cost = 30;
+        dlc = DLC.BASE;
+        tags = new ArrayList<Tag>(Arrays.asList(Tag.JOVIAN, Tag.SPACE));
+    }
+
+    @Override
+    public boolean canPlayCard(){ return super.canPlayCard(); }
+
+    @Override
+    public int getVictoryPointCount() { return 2; }
+
+    @Override
+    public void runImmediateEffect() throws InvalidPlayerTransactionException {
+        PlayerTransaction pt = getImmediateEffectPT();
+        pt.addResource(Resource.TITANIUM_PRODUCTION, 2);
+        pt.execute();
+    }
+}
