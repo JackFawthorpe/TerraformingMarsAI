7db852011b2b5bc5ad006b166507ca64cdfc6a9d
Emma Fawthorpe <emmacansew@gmail.com>
2022-12-24 20:42:02 +1300

    Created Medical Lab Card

---
+++ src/main/Models/ProjectCards/MedicalLabCard.java
@@ -0,0 +1,35 @@
+package main.Models.ProjectCards;
+
+import main.Enums.DLC;
+import main.Enums.Resource;
+import main.Enums.Tag;
+import main.Exceptions.InvalidPlayerTransactionException;
+import main.Managers.CardManager;
+import main.Models.BaseCard;
+import main.Models.PlayerTransaction;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MedicalLabCard extends BaseCard {
+    public MedicalLabCard(CardManager cardManager){
+        this.cardManager = cardManager;
+        title = "Medical Lab";
+        cost = 13;
+        dlc = DLC.CORPORATE;
+        tags = new ArrayList<Tag>(Arrays.asList(Tag.SCIENCE, Tag.BUILDING));
+    }
+
+    @Override
+    public boolean canPlayCard() { return super.canPlayCard(); }
+
+    @Override
+    public int getVictoryPointCount() { return 1; }
+    @Override
+    public void runImmediateEffect() throws InvalidPlayerTransactionException {
+        PlayerTransaction pt = getImmediateEffectPT();
+        int tagCount = owner.getTagCount(Tag.BUILDING, true) / 2;
+        pt.addResource(Resource.CREDIT_PRODUCTION, tagCount);
+        pt.execute();
+    }
+}
